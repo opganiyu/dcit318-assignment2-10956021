110bb9c815449bc902a70930b2be1fa901c23f5c
OlaBisi <OlaBisi@DESKTOP-524TT6O>
2024-06-17 14:11:32 +0000

    #commit Interface

---
+++ Interface/Interface/IMovable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    // Class that implements the interface
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Class that implements the interface
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+}
---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using Interface;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car = new Car();
+        car.Move();
+
+        Bicycle bicycle = new Bicycle();
+        bicycle.Move();
+    }
+}
