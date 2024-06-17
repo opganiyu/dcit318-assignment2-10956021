b6dcce45c5269749bee62b8028e8c62b5146229b
OlaBisi <OlaBisi@DESKTOP-524TT6O>
2024-06-17 14:02:02 +0000

    #commit

---
+++ Inheritance/Inheritance/Animal.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    // Base class with a method
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    // Derived class that overrides the method
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class that overrides the method
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+}
---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,16 @@
+﻿using Inheritance;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Animal animal = new Animal();
+        animal.MakeSound();
+
+        Dog dog = new Dog();
+        dog.MakeSound();
+
+        Cat cat = new Cat();
+        cat.MakeSound();
+    }
+}
