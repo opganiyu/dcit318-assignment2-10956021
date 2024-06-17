4cb970892f9918c530de4de20d9d85d4e463fb09
OlaBisi <OlaBisi@DESKTOP-524TT6O>
2024-06-17 14:13:57 +0000

    #Commit abstruct

---
+++ Abtruct Classes and methods/Abtruct Classes and methods/Program.cs
@@ -0,0 +1,15 @@
+﻿using Abtruct_Classes_and_methods;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Circle circle = new Circle(5.0);
+        Console.WriteLine("Circle area: " + circle.GetArea());
+
+        Rectangle rectangle = new Rectangle(4.0, 6.0);
+        Console.WriteLine("Rectangle area: " + rectangle.GetArea());
+    }
+}
---
+++ Abtruct Classes and methods/Abtruct Classes and methods/Shape.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abtruct_Classes_and_methods
+{
+    // Abstract class with an abstract method
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    // Derived class that implements the abstract method
+    class Circle : Shape
+    {
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    // Derived class that implements the abstract method
+    class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+        public override double GetArea()
+        {
+            return length * width;
+        }
+    }
+}
